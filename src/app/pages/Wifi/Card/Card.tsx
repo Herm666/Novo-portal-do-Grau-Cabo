ba055d042fb682d13e7b32a202c4241d5e47fe89
douglas-front <douglasjknw007@gmail.com>
2024-10-03 13:56:23 -0300

    feat: wifi page finished

--- src/app/pages/Wifi/Card/Card.tsx
+++ src/app/pages/Wifi/Card/Card.tsx
@@ -21,10 +21,18 @@ export default function Card() {
 
                 <div className={styles.wifi}>
                     <h4>rede:</h4>
+
+                    <svg className={styles.arrow} width="1.363vw" height="1.363vw" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M32.5 3C32.5 1.61929 31.3807 0.500001 30 0.499999L7.5 0.5C6.11929 0.5 5 1.61929 5 3C5 4.38071 6.11929 5.5 7.5 5.5H27.5V25.5C27.5 26.8807 28.6193 28 30 28C31.3807 28 32.5 26.8807 32.5 25.5L32.5 3ZM3.76777 32.7678L31.7678 4.76777L28.2322 1.23223L0.232233 29.2322L3.76777 32.7678Z" fill="white" />
+                    </svg>
                     <h4>wifi alunos</h4>
                 </div>
                 <div className={styles.password}>
                     <h4>senha:</h4>
+
+                    <svg className={styles.arrow} width="1.363vw" height="1.363vw" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M32.5 3C32.5 1.61929 31.3807 0.500001 30 0.499999L7.5 0.5C6.11929 0.5 5 1.61929 5 3C5 4.38071 6.11929 5.5 7.5 5.5H27.5V25.5C27.5 26.8807 28.6193 28 30 28C31.3807 28 32.5 26.8807 32.5 25.5L32.5 3ZM3.76777 32.7678L31.7678 4.76777L28.2322 1.23223L0.232233 29.2322L3.76777 32.7678Z" fill="white" />
+                    </svg>
                     <h4>alunograucabo</h4>
                 </div>
 
