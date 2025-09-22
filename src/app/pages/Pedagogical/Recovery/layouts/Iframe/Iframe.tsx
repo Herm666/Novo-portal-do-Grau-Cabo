014c1dfaa6a29b0433375690b409d118b2b26c94
Herm666 <hermesongrauteccabo@gmail.com>
2025-09-22 11:10:22 -0300

    Commit inicial do novo portal do Grau Cabo 01

--- src/app/pages/Pedagogical/Recovery/layouts/Iframe/Iframe.tsx
+++ src/app/pages/Pedagogical/Recovery/layouts/Iframe/Iframe.tsx
@@ -8,7 +8,7 @@ export default function Iframe() {
             <div className={styles.button}>
                 <Button className='btnFrame' text='home'  click={()=> { Transition({to: "/"}) }}/>
             </div>
-            <iframe className={styles.frame} src="http://docs.google.com/forms/d/e/1FAIpQLSc3C2yPsnzDL5jYpbqmg4tay4b-JZG1UvGyarjbDNkKxhgk-Q/viewform"></iframe>
+            <iframe className={styles.frame} src="https://docs.google.com/forms/d/e/1FAIpQLSc3C2yPsnzDL5jYpbqmg4tay4b-JZG1UvGyarjbDNkKxhgk-Q/viewform"></iframe>
         </section>
     )
 }
